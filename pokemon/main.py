1f60e92032046d46c90e2c11d94d263a0ce93d8d
JulienOusmer <julienousmer46090@gmail.com>
2023-10-26 15:30:43 +0200

    connection database SQLite

--- pokemon/main.py
+++ pokemon/main.py
@@ -1,15 +1,46 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import databases
+from databases import Database
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# Étape 2: Connectez-vous à la base de données SQLite en utilisant le module "databases"
+database = Database("sqlite:///./db/pokemon.sqlite")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.on_event("startup")
+async def startup_database():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown_database():
+    await database.disconnect()
+
+
+# Exemple d'un point de l'API pour obtenir tous les types de Pokémon
+@app.get("/types")
+async def get_pokemon_types():
+    query = "SELECT * FROM Type"
+    types = await database.fetch_all(query)
+    if not types:
+        raise HTTPException(status_code=404, detail="Pas de types trouvés")
+    return types
+
+
+# Exemple d'un point de l'API pour obtenir un Pokémon par son ID
+@app.get("/pokemon/{pokemon_id}")
+async def get_pokemon_by_id(pokemon_id: int):
+    query = "SELECT * FROM Pokemon WHERE PokemonID = :pokemon_id"
+    values = {"pokemon_id": pokemon_id}
+    pokemon = await database.fetch_one(query, values)
+    if not pokemon:
+        raise HTTPException(status_code=404, detail="Pokémon not found")
+    return pokemon
+
+
+# Étape 4: Exécutez l'application FastAPI
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
